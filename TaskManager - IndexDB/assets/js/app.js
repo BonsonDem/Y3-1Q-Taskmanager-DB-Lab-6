b6ad1834b07736f1d0430a29609c1882b77998d7
BonsonDem <it.bonson.demssie@gmail.com>
2021-02-08 10:12:01 -0800

    third commit

---
+++ TaskManager - IndexDB/assets/js/app.js
@@ -0,0 +1,162 @@
+const taskInput = document.querySelector('#task');               //the task input text field
+
+const form = document.querySelector('#task-form');             //The form at the top
+
+const filter = document.querySelector('#filter');                      //the task filter text field
+
+const sorting = document.querySelector('#sorting');
+
+const taskList = document.querySelector('.collection');          //The ul
+
+const clearBtn = document.querySelector('.clear-tasks');      //the all task clear button
+
+const reloadIcon = document.querySelector('.fa');
+
+let DB;
+let isSorted = true;
+
+// Event Listener for reload 
+reloadIcon.addEventListener('click', reloadPage);
+
+// Reload Page Function 
+function reloadPage() {
+    //using the reload fun on location object 
+    location.reload();
+}
+form.addEventListener('submit', addNewTask);
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    //all code will reside here 
+    let TasksDB = indexedDB.open("tasks", 1);
+    TasksDB.onsuccess = function (event) {
+        console.log('Database Ready');
+        DB = TasksDB.result;
+        displayTaskList();
+
+    };
+    TasksDB.onerror = function (event) {
+        console.log('There was an error');
+    };
+    TasksDB.onupgradeneeded = function (e) {
+        // the event will be the database
+        let db = e.target.result;
+
+        // create an object store, 
+        // keypath is going to be the Indexes
+        let objectStore = db.createObjectStore('tasks', { keyPath: 'id', autoIncrement: true });
+
+        // createindex: 1) field name 2) keypath 3) options
+        objectStore.createIndex('tasknamez', ['date', 'taskname'], { unique: false });
+        // objectStore.createIndex('created', 'date', { unique: true });
+
+        console.log('Database ready and fields created!');
+    }
+    
+
+
+});
+function addNewTask(e) {
+    // create a new object with the form info
+    e.preventDefault();
+
+    let newTask = {
+        taskname: taskInput.value,
+        date: Date.now()
+    }
+    // Insert the object into the database 
+    let transaction = DB.transaction(['tasks'], 'readwrite');
+    let objectStore = transaction.objectStore('tasks');
+
+    let request = objectStore.add(newTask);
+    // on success
+    request.onsuccess = () => {
+        form.reset();
+    }
+    transaction.oncomplete = () => {
+        console.log('New appointment added');
+        displayTaskList();
+    }
+    transaction.onerror = () => { console.log('There was an error, try again!'); }
+}
+clearBtn.addEventListener('click', clearAllTasks);
+//clear tasks 
+function clearAllTasks() {
+    //Create the transaction and object store
+    let transaction = DB.transaction("tasks", "readwrite");
+    let tasks = transaction.objectStore("tasks");
+
+    // clear the the table
+    tasks.clear();
+    //repaint the UI
+    displayTaskList();
+
+    console.log("Tasks Cleared !!!");
+}
+taskList.addEventListener('click', removeTask);
+
+function removeTask(e) {
+
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are You Sure about that ?')) {
+            // get the task id
+            let taskID = Number(e.target.parentElement.parentElement.getAttribute('data-task-id'));
+            // use a transaction
+            let transaction = DB.transaction('tasks', 'readwrite');
+            let objectStore = transaction.objectStore('tasks');
+            objectStore.delete(taskID);
+
+            transaction.oncomplete = () => {
+                e.target.parentElement.parentElement.remove();
+            }
+
+        }
+    }
+}
+
+
+sorting.addEventListener("change", order);
+function order(e){
+    isSorted = !isSorted;
+    displayTaskList(e)
+}
+function displayTaskList(e){
+    // clear the previous task list
+    while (taskList.firstChild) { taskList.removeChild(taskList.firstChild); }
+
+    // create the object store
+    let objectStore = DB.transaction('tasks').objectStore('tasks').index('tasknamez');
+    let param1;
+    let param2;
+    if(isSorted) {
+        param1 = null; 
+        param2 = 'next';
+    }
+    else{
+        param1 = null;
+        param2 = 'prev'
+    }
+    objectStore.openCursor(param1, param2).onsuccess = function (e) {
+        // assign the current cursor
+        let cursor = e.target.result;
+
+        if (cursor) {
+            const li = document.createElement('li');
+            // Adding a class
+            li.className = 'collection-item';
+            // Create new element for the link 
+            const link = document.createElement('a');
+            // Add class and the x marker for a 
+            link.className = 'delete-item secondary-content';
+            link.innerHTML = `<i class="fa fa-remove"></i> &nbsp;<a href="./../edit.html?id=${cursor.value.id}"><i class="fa fa-edit"></i> </a> `;
+            // Append link to li
+
+            li.appendChild(link);
+            li.setAttribute('data-task-id', cursor.value.id);
+            // Create text node and append it 
+            li.appendChild(document.createTextNode(cursor.value.taskname));
+            taskList.appendChild(li);
+            cursor.continue();
+        }
+    }
+}
---
+++ TaskManager - IndexDB/assets/js/edit.js
@@ -0,0 +1,117 @@
+//UI variables 
+const form = document.querySelector('#task-form'); //The form at the top
+const taskInput = document.querySelector('#task'); //the task input text field
+
+//read from q string 
+const urlParams = new URLSearchParams(window.location.search);
+const id = Number(urlParams.get('id'));
+//DB
+var DB;
+
+// Add Event Listener [on Load]
+document.addEventListener('DOMContentLoaded', () => {
+    // create the database
+    let TasksDB = indexedDB.open('tasks', 1);
+
+    // if there's an error
+    TasksDB.onerror = function () {
+        console.log('There was an error');
+    }
+    // if everything is fine, assign the result to the instance
+    TasksDB.onsuccess = function () {
+        // console.log('Database Ready');
+
+        // save the result
+        DB = TasksDB.result;
+
+        // display the Task 
+        displayTask();
+    }
+
+
+    function displayTask() {
+
+        var transaction = DB.transaction(['tasks']);
+        var objectStore = transaction.objectStore('tasks');
+        var request = objectStore.get(id);
+
+        request.onsuccess = function (event) {
+            if (request.result) {
+                taskInput.value = request.result.taskname;
+
+            } else {
+                console.log('No data record');
+            }
+        };
+
+        request.onerror = function (event) {
+            console.log('Transaction failed');
+        };
+
+
+
+    }
+
+
+    form.addEventListener('submit', updateTask);
+
+    function updateTask(e) {
+        e.preventDefault();
+
+
+        // Check empty entry
+        if (taskInput.value === '') {
+            taskInput.style.borderColor = "red";
+
+            return;
+        }
+
+        /* 
+        Instruction set to handle Update
+        1. Declare the transaction and object store objects 
+        2. Use the id on put method of index db
+        
+        */
+        // use a transaction
+        let transaction = DB.transaction('tasks', 'readwrite');
+        let objectStore = transaction.objectStore('tasks');
+        let req = objectStore.openCursor()
+        req.onerror = function (e) {
+            console.log("case if have an error");
+        };
+        req.onsuccess = function (e) {
+            let cursor = e.target.result;
+            let newTask = {
+                taskname: taskInput.value,
+                date: Date.now(),
+                id: id
+            }
+
+            if (cursor) {
+                
+                if(cursor.value.id == id){
+                    
+                
+                var res = cursor.update(newTask);
+                res.onsuccess = function (e) {
+                    console.log("update success!!");
+                }
+                res.onerror = function (e) {
+                    console.log("update failed!!");
+                }
+
+            }
+            cursor.continue();
+        }
+        else {
+            console.log("fin mise a jour");
+        }
+            // objectStore.put(newTask, newTask);
+
+        history.back();
+        }
+        
+    }
+
+
+});
